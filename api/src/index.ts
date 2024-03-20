662ac00c6cb5e05fbcfa76a337a074e37f6d11a3
Jake Gill <jake.w.g21@gmail.com>
2024-03-20 15:05:55 -0400

    Changed the PORT
    Change default port from 3000 to 8000 because the frontend runs on 3000 already.

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,6 @@ import { server } from './config/server';
 
 setupDatabase();
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port 8000');
 });
